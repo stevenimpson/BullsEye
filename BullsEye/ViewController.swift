954f319d621216fbd984ac8c07845cbfb430fd50
Steven Impson <steven.impson@gmail.com>
2018-08-06 21:42:47 +0930

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Steven Impson on 14/7/18.
+//  Copyright © 2018 Steven Impson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var currentValue = 50
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startNewGame()    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+
+    @IBAction func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    
+    @IBAction func showAlert() {
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let message = "You scored \(points) points!"
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default,
+                                   handler: { action in self.startNewRound()})
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider:UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+}
+
